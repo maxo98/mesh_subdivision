b705e46608e19f7fb661bec7d55be23c9cf51fe1
Mickaël BERTHELOT <41340047+crread@users.noreply.github.com>
2023-06-06 14:07:14 +0200

    [Update] Fixe segmentations

--- Assets/Scripts/CurveCornerCuttingAlgo.cs
+++ Assets/Scripts/CurveCornerCuttingAlgo.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Debug = UnityEngine.Debug;
 
 public class CurveCornerCuttingAlgo : MonoBehaviour
 {
@@ -36,10 +39,10 @@ public class CurveCornerCuttingAlgo : MonoBehaviour
         _idDirectionSelected = DirectionSelected.Front;
         _listPointsByFace = new Dictionary<DirectionSelected, List<GameObject>>
         {
-            [DirectionSelected.Front] = new List<GameObject>(),
-            [DirectionSelected.Back] = new List<GameObject>(),
-            [DirectionSelected.Left] = new List<GameObject>(),
-            [DirectionSelected.Right] = new List<GameObject>()
+            [DirectionSelected.Front] = new List<GameObject>(numberOfPoints),
+            [DirectionSelected.Back] = new List<GameObject>(numberOfPoints),
+            [DirectionSelected.Left] = new List<GameObject>(numberOfPoints),
+            [DirectionSelected.Right] = new List<GameObject>(numberOfPoints)
         };
         
         _listPointsSegmented = new Dictionary<DirectionSelected, List<GameObject>>
@@ -132,31 +135,69 @@ public class CurveCornerCuttingAlgo : MonoBehaviour
         if (!Physics.Raycast(ray, out var hit, Mathf.Infinity, layerMask, QueryTriggerInteraction.Ignore) || _listPointsByFace[_idDirectionSelected].Count >= numberOfPoints)
             return;
 
-        InstantiatePoint(hit.point, defaultPointMaterial, _listPointsByFace[_idDirectionSelected]);
+        if ((_idDirectionSelected == DirectionSelected.Right || _idDirectionSelected == DirectionSelected.Left) &&
+            _listPointsByFace[_idDirectionSelected].Count == 4)
+        {
+            if (_idDirectionSelected == DirectionSelected.Right)
+                _listPointsByFace[_idDirectionSelected].Add(_listPointsByFace[DirectionSelected.Back][0]);
+            else
+                _listPointsByFace[_idDirectionSelected].Add(_listPointsByFace[DirectionSelected.Front][0]);
+        }
+        else 
+            InstantiatePoint(hit.point, defaultPointMaterial, _listPointsByFace[_idDirectionSelected]);
+
+        switch (_idDirectionSelected)
+        {
+            case DirectionSelected.Front when _listPointsByFace[_idDirectionSelected].Count == numberOfPoints:
+                _listPointsByFace[DirectionSelected.Right].Add(_listPointsByFace[_idDirectionSelected][numberOfPoints - 1]);
+                break;
+            case DirectionSelected.Back when _listPointsByFace[_idDirectionSelected].Count == numberOfPoints:
+                _listPointsByFace[DirectionSelected.Left].Add(_listPointsByFace[_idDirectionSelected][numberOfPoints - 1]);
+                break;
+        }
+    }
+
+    public void OnClickSwitchCameraFace()
+    {
+        if (_idDirectionSelected == DirectionSelected.Right)
+            return;
+
+        switch (_idDirectionSelected + 1)
+        {
+            case DirectionSelected.Left:
+                OnClickMoveCameraLeftSide();
+                break;
+            case DirectionSelected.Right:
+                OnClickMoveCameraRightSide();
+                break;
+            case DirectionSelected.Back:
+                OnClickMoveCameraBackSide();
+                break;
+        }
     }
     
-    public void OnClickMoveCameraRightSide()
+    private void OnClickMoveCameraRightSide()
     {
         mainCamera.transform.position = new Vector3(20, 0, 0);
         mainCamera.transform.rotation = Quaternion.Euler(0, -90, 0);
         _idDirectionSelected = DirectionSelected.Right;
     }
 
-    public void OnClickMoveCameraLeftSide()
+    private void OnClickMoveCameraLeftSide()
     {
         mainCamera.transform.position = new Vector3(-20, 0, 0);
         mainCamera.transform.rotation = Quaternion.Euler(0, 90, 0);
         _idDirectionSelected = DirectionSelected.Left;
     }
 
-    public void OnClickMoveCameraFrontSide()
+    private void OnClickMoveCameraFrontSide()
     {
         mainCamera.transform.position = new Vector3(0, 0, -20);
         mainCamera.transform.rotation = Quaternion.Euler(0, 0, 0);
         _idDirectionSelected = DirectionSelected.Front;
     }
 
-    public void OnClickMoveCameraBackSide()
+    private void OnClickMoveCameraBackSide()
     {
         mainCamera.transform.position = new Vector3(0, 0, 20);
         mainCamera.transform.rotation = Quaternion.Euler(0, 180, 0);
